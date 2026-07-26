64122588ed6700574bb001661ef6f11fab14c220
Xavier <tim88776@gmail.com>
2026-07-26 12:02:31 +0800

    feat: add repl backbone

--- repl.go
+++ repl.go
@@ -1,6 +1,32 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		input := cleanInput(scanner.Text())
+		if len(input) < 1 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", input[0])
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func cleanInput(text string) []string {
 	// split the user's input into words based on whitespace
